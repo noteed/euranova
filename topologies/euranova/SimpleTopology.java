46289c231dc7008335b842c0cca5b86ce025f221
Vo Minh Thu <thu@hypered.io>
2014-10-14 15:44:20 +0200

    Add code for the TestWordSpout class.
    Include the code for the backtype.storm.testing.TestWordSpout class. This will
    serve as a basis for a spout emitting (model, items) tuples.

--- topologies/euranova/SimpleTopology.java
+++ topologies/euranova/SimpleTopology.java
@@ -20,23 +20,61 @@ package euranova;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
-import backtype.storm.testing.TestWordSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 
 import java.util.Map;
+import java.util.Random;
 
 /**
  * This is a basic example of a Storm topology.
  */
 public class SimpleTopology {
+
+  public static class TestWordSpout extends BaseRichSpout {
+    SpoutOutputCollector _collector;
+
+    public TestWordSpout() {
+      this(true);
+    }
+
+    public TestWordSpout(boolean isDistributed) {
+    }
+
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+      _collector = collector;
+    }
+
+    public void close() {
+    }
+
+    public void nextTuple() {
+      Utils.sleep(100);
+      final String[] words = new String[] {"nathan", "mike", "jackson", "golda", "bertels"};
+      final Random rand = new Random();
+      final String word = words[rand.nextInt(words.length)];
+      _collector.emit(new Values(word));
+    }
+
+    public void ack(Object msgId) {
+    }
+
+    public void fail(Object msgId) {
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("word"));
+    }
+  }
 
   public static class ExclamationBolt extends BaseRichBolt {
     OutputCollector _collector;
